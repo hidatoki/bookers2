a8e0551c16877b221c7dcd8784e73c36ae5b0cbc
EC2 Default User <ec2-user@ip-172-31-74-125.ec2.internal>
2021-12-25 18:09:24 +0000

    [finish]book-index-left_side_page & [fix]sign_in後のページ遷移とヘッダーのリンク先ルート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,35 @@
 class BooksController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
   end
-  
+
   def index
+    @users = User.all
+    @user = User.find_by(id: params[:user_id])
     @books = Book.all
   end
-  
+
   def show
+    @user = User.find(params[:id])
   end
-  
+
   def edit
 
-  end 
-  
+  end
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :body)
+  end
+
 end
