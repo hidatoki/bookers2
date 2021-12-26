0f95c72b20217496acb02c85536495dd1685dec6
EC2 Default User <ec2-user@ip-172-31-74-125.ec2.internal>
2021-12-26 01:39:35 +0000

    [finish]book-index-right_side_page & [add]booksの部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(id: current_user)
   end
 
   def index
@@ -13,7 +17,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:user_id])
   end
 
   def edit
@@ -30,6 +34,10 @@ class BooksController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :body)
+  end
+  
+  def book_params
+    params.permit(:title, :body)
   end
 
 end
